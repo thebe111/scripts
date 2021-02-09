d4221d29087f4cbcb32f1378b7333a24b5e9537f
Gabriel Fonseca <fonsecacrz7@gmail.com>
2021-02-09 20:49:15 -0300

    feat: added notify-send as notifier daemon

--- notifications.py
+++ notifications.py
@@ -3,13 +3,14 @@
 # DISPLAY=:0
 # */1 * * * * export $(dbus-launch) && python3 ~/scripts/notifications.py > /tmp/notifications.log 2>&1
 
-import gi
+#  import gi
 
-gi.require_version("Notify", "0.7")
-from gi.repository import Notify
+#  gi.require_version("Notify", "0.7")
+#  from gi.repository import Notify
 
+#  Notify.init("Notifications Script")
 
-Notify.init("Notifications Script")
+import os
 
 # ----------------------------
 # VERIFY BATTERY STATUS
@@ -18,8 +19,10 @@ with open("/sys/class/power_supply/BAT0/capacity", "r") as battery_level:
     if int(battery_level.read()) < 20:
         with open("/sys/class/power_supply/BAT0/status", "r") as battery_status:
             if battery_status.read().replace('\n', '') == "Discharging":
-                battery_notification = Notify.Notification.new(
-                    "BATTERY STATUS", "Connect your charger"
-                )
-                battery_notification.set_urgency(Notify.Urgency.CRITICAL)
-                battery_notification.show()
+                os.system("notify-send 'LOW BATTERY' -u critical -a system")
+
+                #  battery_notification = Notify.Notification.new(
+                    #  "BATTERY STATUS", "Connect your charger"
+                #  )
+                #  battery_notification.set_urgency(Notify.Urgency.CRITICAL)
+                #  battery_notification.show()
